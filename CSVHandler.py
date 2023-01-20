67ee69031ef9c15229a116214524904514972c3d
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-20 10:12:48 +0100

    delete user

--- CSVHandler.py
+++ CSVHandler.py
@@ -22,18 +22,34 @@ class CSVHandler:
 
         return all_data
 
+    
+    @staticmethod
+    def get_all_data():
+        all_data = []
+        with open("data.csv", newline="") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                all_data.append(row)
+
+        return all_data
+
+
     @staticmethod
     def delete_all_row(row_to_delete):
-        all_data = CSVHandler.read_all_data()
-        print(all_data)
-        # for i in range(len(all_data)):
-        #    if all_data[i]["user"] == row_to_delete:
-        #        all_data.remove(all_data[i])
+        all_data = CSVHandler.get_all_data()
+        # print(all_data)
+        new_data = []
+        for i in range(len(all_data)):
+           if all_data[i]["user"] != row_to_delete:
+            #    all_data.remove(all_data[i])
+               new_data.append(all_data[i])
+            
+        header = ["user", "score"]
+        print(new_data)
 
-        # header = ["user", "score"]
-
-        # with open("data.csv", "w") as f:
-        #     writer = csv.DictWriter(f, fieldnames=header)
-        #     writer.writerow(all_data)
-        #     f.close()
+        with open("data.csv", "w") as f:
+            writer = csv.DictWriter(f, fieldnames=header)
+            writer.writeheader()
+            writer.writerows(new_data)
+            f.close()
 
